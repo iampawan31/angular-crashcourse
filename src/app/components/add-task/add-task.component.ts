8a036d49e46ae98b81c479bcd2825b940ab52bcc
Pawan Kumar <surpawan@gmail.com>
2021-09-21 21:09:03 +0530

    Added Logic

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css'],
+})
+export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task');
+      return;
+    }
+
+    const newTask = {
+      id: 10,
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
+}
